469258cdafbbc8c4c13dd8e24cf222cd4d42fb73
Miguel Ángel Linares Martínez <malinmar@upv.edu.es>
2022-12-16 11:12:27 +0100

    Mapa hecho

---
+++ frontend-iei/src/MapWrapper.jsx
@@ -0,0 +1,84 @@
+import './MapWrapper.css'
+
+// react
+import React, { useState, useEffect, useRef } from 'react';
+import SplitPane from 'react-split-pane';
+
+// openlayers
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import XYZ from 'ol/source/XYZ'
+import {transform} from 'ol/proj'
+import {toStringXY} from 'ol/coordinate';
+import OSM from 'ol/source/OSM';
+import Overlay from 'ol/Overlay';
+import TileJSON from 'ol/source/TileJSON';
+
+function MapWrapper() {
+
+    // set intial state
+    const [ map, setMap ] = useState(new Map())
+    const [ featuresLayer, setFeaturesLayer ] = useState()
+
+    // pull refs
+    const mapElement = useRef()
+
+     // create state ref that can be accessed in OpenLayers onclick callback function
+    //  https://stackoverflow.com/a/60643670
+    const mapRef = useRef()
+    mapRef.current = map
+
+    useEffect( () => {
+
+        // create and add vector source layer
+        const initalFeaturesLayer = new VectorLayer({
+            source: new VectorSource()
+        })
+
+        const initialMap = new Map({
+            target: mapElement.current,
+            layers: [
+              
+              // USGS Topo
+              // new TileLayer({
+              //   source: new XYZ({
+              //     url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
+              //   })
+              // }),
+      
+              // Google Maps Terrain
+              //Mapa OSM
+               new TileLayer({
+                   source: new OSM()
+                 }),
+      
+              initalFeaturesLayer
+              
+            ],
+            view: new View({
+              projection: 'EPSG:3857',
+              center: [-41963.928529, 4789727.64075],
+              zoom: 10
+              
+            }),
+            controls: []
+          })
+
+
+        // save map and vector layer references to state
+        setMap(initialMap)
+        setFeaturesLayer(initalFeaturesLayer)
+    },[]);
+
+    // render component
+  return (
+    <div>
+        <div ref={mapElement} className="map-container"></div>
+    </div> 
+  ) 
+}
+
+export default MapWrapper
--- frontend-iei/src/main.jsx
+++ frontend-iei/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import SplitPane from 'react-split-pane';
+
 import App from './App'
+import MapWrapper from './MapWrapper'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SplitPane split="vertical" defaultSize="45%">
+      <div className='Izquierda'>
+        <h1>hola</h1>
+      </div>
+      <div className='Derecha'>
+        <MapWrapper/>
+      </div>
+    </SplitPane>
+    
   </React.StrictMode>,
 )
